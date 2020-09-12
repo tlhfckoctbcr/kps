351534b2130f335784f9232a8e328ead9af672a9
grantsru <grantsru@gmail.com>
2020-09-11 22:22:20 -0400

    Test out components, prep styled-components

---
+++ src/components/Circle.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const StyledCircle = styled.div`
+  background: black;
+  height: 500px;
+  width: 100%;
+`;
+
+const Circle = () => {
+  return <StyledCircle />;
+}
+
+export default Circle;
