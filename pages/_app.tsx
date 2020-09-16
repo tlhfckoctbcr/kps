c85f67007294dd42a1d7eacf688af7985a296848
grantsru <grantsru@gmail.com>
2020-09-15 22:45:14 -0400

    Setup custom fonts w/ styled-components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
-import '../public/global.css';
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'GrenzeGotisch';
+    src: url('/static/fonts/GrenzeGotisch-Regular.ttf');
+    src: url('/static/fonts/GrenzeGotisch-Bold.ttf');
+    src: url('/static/fonts/GrenzeGotisch-Black.ttf');
+    src: url('/static/fonts/GrenzeGotisch-ExtraLight.ttf');
+  }
+  html, body {
+    margin: 0;
+    padding: 0;
+    font-family: 'GrenzeGotisch';
+  }
+`;
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Component {...pageProps} />
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
   );
 }
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,33 +1,29 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document';
-import { ServerStyleSheet } from 'styled-components';
+import Document from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
-  static getInitialProps({ renderPage }) {
+  static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
-    const page = renderPage((App) => (props) =>
-      sheet.collectStyles(<App {...props} />),
-    );
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+        });
 
-    const styleTags = sheet.getStyleElement();
-
-    return { ...page, styleTags };
-  }
-
-  render() {
-    // @ts-ignore
-    const { styleTags } = this.props;
-
-    return (
-      <Html>
-        <Head>
-          {styleTags}
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        )
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 }
